51feeddf71f68c80a30c4d4407f0d0f13fb8e9b0
Douglas Rolim <douglasrolim@gmail.com>
2020-05-21 16:33:40 -0300

    update

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/query')
+mongoose.connect('mongodb://srv-bigdata05.mp.rn.gov.br/saveLayer')
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
